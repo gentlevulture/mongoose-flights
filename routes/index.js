f5c8d67a390d475683f34c7c9c6eebafd3bd61a6
gentlevulture <novemberboughs@gmail.com>
2021-11-02 06:37:05 -0700

    addAndEditCSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 const router = Router()
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mongoose Flights' })
 })
